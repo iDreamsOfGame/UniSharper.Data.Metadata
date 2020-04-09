fd9ee3654abd5a87c17295d3a02a992a0eb50f35
Jerry Lee <cosmos53076@163.com>
2020-04-09 15:37:25 +0800

    Add package scripts.

---
+++ Assets/UniSharper.Data.Metadata/Scripts/Editor/Converters/IPropertyTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Jerry Lee. All rights reserved. Licensed under the MIT License. See LICENSE in the
+// project root for license information.
+
+namespace UniSharper.Data.Metadata.Parsers
+{
+    /// <summary>
+    /// The interface of property type converter.
+    /// </summary>
+    internal interface IPropertyTypeConverter
+    {
+        #region Methods
+
+        /// <summary>
+        /// Parses the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The parsed data.</returns>
+        object Parse(string value, params object[] parameters);
+
+        #endregion Methods
+    }
+}
