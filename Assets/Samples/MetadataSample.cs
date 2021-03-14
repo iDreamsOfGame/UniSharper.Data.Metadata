bc300111aa4e6c072517393f8b1c6d83c3922cc8
Jerry Lee <cosmos53076@163.com>
2021-03-14 15:46:45 +0800

    Refactor codes

---
+++ Assets/Samples/MetadataSample.cs
@@ -0,0 +1,77 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UniSharper.Data.Metadata.Samples
+{
+    public class MetadataSample : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private Canvas canvas = null;
+
+        #endregion Fields
+
+        #region Properties
+
+        private MetadataManager MetadataManager => MetadataManager.Instance;
+
+        #endregion Properties
+
+        #region Methods
+
+        private void AddText(string text)
+        {
+            var go = new GameObject
+            {
+                name = "Text",
+                layer = LayerMask.NameToLayer("UI")
+            };
+            var textFiled = go.AddComponent<Text>();
+            textFiled.font = Font.CreateDynamicFontFromOSFont("Arial", 32);
+            textFiled.text = text;
+            textFiled.alignment = TextAnchor.MiddleCenter;
+            go.transform.SetParent(canvas.transform, false);
+        }
+
+        private void Awake()
+        {
+            var binAsset = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Metadata/Data/MetadataEntityDBConfig.db.bytes");
+            MetadataManager.Initialize(binAsset.bytes);
+
+            // Load DB data of EmapleMetadata
+            binAsset = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Metadata/Data/SampleMetadata.db.bytes");
+            MetadataManager.LoadEntityDatabase<SampleMetadata>(binAsset.bytes);
+
+            // Load DB data of TestMetadata
+            binAsset = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Metadata/Data/TestMetadata.db.bytes");
+            MetadataManager.LoadEntityDatabase<TestMetadata>(binAsset.bytes);
+        }
+
+        private void OnDestroy()
+        {
+            MetadataManager.Dispose();
+        }
+
+        private void Start()
+        {
+            var metadata = MetadataManager.GetEntity<SampleMetadata>(1L);
+            AddText(metadata.TestString);
+            AddText(metadata.TestEnum.ToString());
+            Debug.Log(MetadataManager.GetEntity<SampleMetadata>(2L));
+
+            var testMetadata = MetadataManager.GetEntity<TestMetadata>(2L);
+            AddText(testMetadata.Name);
+
+            var exampleMetadatas = MetadataManager.GetAllEntities<SampleMetadata>();
+            AddText(string.Format("The count of entity 'ExampleMetadata': {0}", exampleMetadatas.Length));
+
+            var testMetadataList = MetadataManager.GetEntities<TestMetadata>("Age", 30);
+
+            Debug.Log(testMetadataList.Length);
+        }
+
+        #endregion Methods
+    }
+}
--- update_package_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-readonly WORK_DIR=$(cd $(dirname $0); pwd)
-cd $WORK_DIR
-openupm add io.github.idreamsofgame.resharp.data.iboxdb
-openupm add com.github.exceldatareader.exceldatareader
-openupm add io.github.idreamsofgame.unisharper.core
